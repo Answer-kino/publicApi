c48319558fc4ed80408c242ff2e6c01f3efdf757
Answer-kino <kino@ianswer.co.kr>
2022-07-13 12:14:09 +0900

    :world_map:Route: shp2geojson

--- src/route/api.ts
+++ src/route/api.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import apiController from "src/controller/apiController";
+import { shpFile } from "src/middleware/uploadHandler";
 
 const router = express.Router();
 
 router.get("/nsdi", apiController.nsdi);
+router.post("/shp2geojson", shpFile.fields([{ name: "shpFile", maxCount: 1 }]), apiController.shp2GeoJson);
 
 export = router;
